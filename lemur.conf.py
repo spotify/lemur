2e3205cb3754d17b6f2d220a1858a05927c43a8b
Benjamin Greschbach <bgre@spotify.com>
2020-12-21 13:47:02 +0100

    revert formatting changes for a cleaner PR

--- lemur.conf.py
+++ lemur.conf.py
@@ -1,7 +1,8 @@
-import base64
 import os
+import random
 import secrets
 import string
+import base64
 from ast import literal_eval
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,63 +15,48 @@ def get_random_secret(length):
     special_chars = "~!@#$%^&*()_+"
     alphabet = string.ascii_letters + string.digits + special_chars
     while True:
-        secret_key = "".join(secrets.choice(alphabet) for i in range(length))
+        secret = "".join(secrets.choice(alphabet) for i in range(length))
         if (
-            any(c.islower() for c in secret_key)
-            and any(c.isupper() for c in secret_key)
-            and any(c.isdigit() for c in secret_key)
-            and any(c in special_chars for c in secret_key)
+            any(c.islower() for c in secret)
+            and any(c.isupper() for c in secret)
+            and any(c.isdigit() for c in secret)
+            and any(c in special_chars for c in secret)
         ):
             break
-    return secret_key
+    return secret
 
 
-SECRET_KEY = repr(os.environ.get("SECRET_KEY", get_random_secret(32).encode("utf8")))
+SECRET_KEY = repr(os.environ.get('SECRET_KEY', get_random_secret(32).encode('utf8')))
 
-LEMUR_TOKEN_SECRET = repr(
-    os.environ.get(
-        "LEMUR_TOKEN_SECRET", base64.b64encode(get_random_secret(32).encode("utf8"))
-    )
-)
-LEMUR_ENCRYPTION_KEYS = repr(
-    os.environ.get(
-        "LEMUR_ENCRYPTION_KEYS", base64.b64encode(get_random_secret(32).encode("utf8"))
-    )
-)
+LEMUR_TOKEN_SECRET = repr(os.environ.get('LEMUR_TOKEN_SECRET',
+                                         base64.b64encode(get_random_secret(32).encode('utf8'))))
+LEMUR_ENCRYPTION_KEYS = repr(os.environ.get('LEMUR_ENCRYPTION_KEYS',
+                                            base64.b64encode(get_random_secret(32).encode('utf8'))))
 
 LEMUR_ALLOWED_DOMAINS = []
 
-LEMUR_EMAIL = ""
+LEMUR_EMAIL = ''
 LEMUR_SECURITY_TEAM_EMAIL = []
 
-ALLOW_CERT_DELETION = os.environ.get("ALLOW_CERT_DELETION") == "True"
+ALLOW_CERT_DELETION = os.environ.get('ALLOW_CERT_DELETION') == "True"
 
-LEMUR_DEFAULT_COUNTRY = str(os.environ.get("LEMUR_DEFAULT_COUNTRY", ""))
-LEMUR_DEFAULT_STATE = str(os.environ.get("LEMUR_DEFAULT_STATE", ""))
-LEMUR_DEFAULT_LOCATION = str(os.environ.get("LEMUR_DEFAULT_LOCATION", ""))
-LEMUR_DEFAULT_ORGANIZATION = str(os.environ.get("LEMUR_DEFAULT_ORGANIZATION", ""))
-LEMUR_DEFAULT_ORGANIZATIONAL_UNIT = str(
-    os.environ.get("LEMUR_DEFAULT_ORGANIZATIONAL_UNIT", "")
-)
+LEMUR_DEFAULT_COUNTRY = str(os.environ.get('LEMUR_DEFAULT_COUNTRY',''))
+LEMUR_DEFAULT_STATE = str(os.environ.get('LEMUR_DEFAULT_STATE',''))
+LEMUR_DEFAULT_LOCATION = str(os.environ.get('LEMUR_DEFAULT_LOCATION',''))
+LEMUR_DEFAULT_ORGANIZATION = str(os.environ.get('LEMUR_DEFAULT_ORGANIZATION',''))
+LEMUR_DEFAULT_ORGANIZATIONAL_UNIT = str(os.environ.get('LEMUR_DEFAULT_ORGANIZATIONAL_UNIT',''))
 
-LEMUR_DEFAULT_ISSUER_PLUGIN = str(os.environ.get("LEMUR_DEFAULT_ISSUER_PLUGIN", ""))
-LEMUR_DEFAULT_AUTHORITY = str(os.environ.get("LEMUR_DEFAULT_AUTHORITY", ""))
+LEMUR_DEFAULT_ISSUER_PLUGIN = str(os.environ.get('LEMUR_DEFAULT_ISSUER_PLUGIN',''))
+LEMUR_DEFAULT_AUTHORITY = str(os.environ.get('LEMUR_DEFAULT_AUTHORITY',''))
 
 METRIC_PROVIDERS = []
 
-LOG_LEVEL = str(os.environ.get("LOG_LEVEL", "DEBUG"))
+ACTIVE_PROVIDERS = ['google']
+
+GOOGLE_CLIENT_ID = str(os.environ.get('GOOGLE_CLIENT_ID','421791425557-lfk56lqi3rnhi4n2fr2rakmbv4lbc93l.apps.googleusercontent.com'))
+GOOGLE_SECRET = str(os.environ.get('GOOGLE_SECRET',''))
+
+LOG_LEVEL = str(os.environ.get('LOG_LEVEL','DEBUG'))
 # LOG_FILE = str(os.environ.get('LOG_FILE','/home/lemur/.lemur/lemur.log'))
 
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "SQLALCHEMY_DATABASE_URI", "postgresql://lemur:lemur@localhost:5432/lemur"
-)
-
-LDAP_DEBUG = os.environ.get("LDAP_DEBUG") == "True"
-LDAP_AUTH = os.environ.get("LDAP_AUTH") == "True"
-LDAP_IS_ACTIVE_DIRECTORY = os.environ.get("LDAP_IS_ACTIVE_DIRECTORY") == "True"
-LDAP_BIND_URI = str(os.environ.get("LDAP_BIND_URI", ""))
-LDAP_BASE_DN = str(os.environ.get("LDAP_BASE_DN", ""))
-LDAP_EMAIL_DOMAIN = str(os.environ.get("LDAP_EMAIL_DOMAIN", ""))
-LDAP_USE_TLS = str(os.environ.get("LDAP_USE_TLS", ""))
-LDAP_REQUIRED_GROUP = str(os.environ.get("LDAP_REQUIRED_GROUP", ""))
-LDAP_GROUPS_TO_ROLES = literal_eval(os.environ.get("LDAP_GROUPS_TO_ROLES") or "{}")
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI','postgresql://lemur:lemur@localhost:5432/lemur')
