302c5fe17375c557d543020440a1380d8fd78c63
Viktor Ahlqvist <viktora@spotify.com>
2020-12-23 10:21:55 +0100

    Add linter pylint to prevent simple mistakes in the code
    Ignore most checks about code style, but let it sort out most common
    errors.

--- spotify-dest-gcp/plugin_dest_gcp/plugin.py
+++ spotify-dest-gcp/plugin_dest_gcp/plugin.py
@@ -1,38 +1,39 @@
-from lemur.plugins.bases import DestinationPlugin
-from flask import current_app
+from flask import current_app  # pylint: disable=import-error
+from lemur.plugins.bases import \
+    DestinationPlugin  # pylint: disable=import-error
 
 from .gcp import Gcp
 
-class GcpDestination(DestinationPlugin):
-    title = 'Gcp Destination'
-    slug = 'gcp_destination'
-    description = 'Gcp Destination Plugin'
 
-    author = 'Your Name'
-    author_url = 'https://github.com/yourname/lemur_pluginname'
+class GcpDestination(DestinationPlugin):
+    title = "Gcp Destination"
+    slug = "gcp_destination"
+    description = "Gcp Destination Plugin"
+
+    author = "Your Name"
+    author_url = "https://github.com/yourname/lemur_pluginname"
 
     options = [
         {
-            'name': 'gcp-project',
-            'type': 'str',
-            'required': True,
+            "name": "gcp-project",
+            "type": "str",
+            "required": True,
         },
         {
-            'name': 'target-proxy-name',
-            'type': 'str',
-            'required': True,
+            "name": "target-proxy-name",
+            "type": "str",
+            "required": True,
         },
     ]
     additional_options = []
 
-    def __init__(self, *args, **kwargs):
-        super(GcpDestination, self).__init__(*args, **kwargs)
-
-
-    def upload(self, name, body, private_key, cert_chain, options, **kwargs):
+    def upload(
+        self, name, body, private_key, cert_chain, options, **kwargs
+    ):  # pylint: disable=unused-argument
         gcp = Gcp(
             self.get_option("gcp-project", options),
             self.get_option("target-proxy-name", options),
-            logger=current_app.logger)
+            logger=current_app.logger,
+        )
 
         gcp.add_certificate(name, body, private_key, cert_chain)
