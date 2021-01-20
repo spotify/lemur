374ba867ab29d6d9a0c90e7fa9b6d14e5ac46cd7
Jonas Palm <jonaspalm@spotify.com>
2021-01-20 12:25:58 +0100

    Emit metrics

--- lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
+++ lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import shumway
 from requests.exceptions import ConnectionError
 from datetime import datetime
 
@@ -15,9 +16,12 @@ class FFWDMetricPlugin(MetricPlugin):
     author = "wasabi@spotify.com"
     author_url = "spotify.com"
 
+    def __init__(self, *args, **kwargs):
+        self.mr = shumway.MetricRelay("lemur")
+
     def submit(
         self, metric_name, metric_type, metric_value, metric_tags=None, options=None
     ):
-        current_app.logger.warning(
-            "ffwd-plugin: not implemented"
-        )
+        current_app.logger.debug(
+            f"ffwd-plugin: sending metrics {metric_name} {metric_type} {metric_value} {metric_tags}"        )
+        self.mr.emit(metric_name, metric_value, tags=metric_tags)
