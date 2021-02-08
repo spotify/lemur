13facf30fc76286f6e353e675f5257e0a9bf3dc2
Jonas Palm <jonaspalm@spotify.com>
2021-02-08 16:34:19 +0100

    Removed unused and format code

--- lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
+++ lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
@@ -1,8 +1,4 @@
-import json
-import requests
 import shumway
-from requests.exceptions import ConnectionError
-from datetime import datetime
 
 from flask import current_app
 from lemur.plugins.bases.metric import MetricPlugin
@@ -19,9 +15,8 @@ class FFWDMetricPlugin(MetricPlugin):
     def __init__(self, *args, **kwargs):
         self.mr = shumway.MetricRelay("lemur")
 
-    def submit(
-        self, metric_name, metric_type, metric_value, metric_tags=None, options=None
-    ):
+    def submit(self, metric_name, metric_type, metric_value, metric_tags=None, options=None):
         current_app.logger.debug(
-            f"ffwd-plugin: sending metrics {metric_name} {metric_type} {metric_value} {metric_tags}"        )
+            f"ffwd-plugin: sending metrics {metric_name} {metric_type} {metric_value} {metric_tags}"
+        )
         self.mr.emit(metric_name, metric_value, tags=metric_tags)
