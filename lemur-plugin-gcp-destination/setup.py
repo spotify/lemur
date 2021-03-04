915b20bc4570e084da68c0d6b5ae44ccb3c713bf
Jonas Palm <jonaspalm@spotify.com>
2021-03-04 15:02:17 +0100

    Pin major version

--- lemur-plugin-gcp-destination/setup.py
+++ lemur-plugin-gcp-destination/setup.py
@@ -10,9 +10,9 @@ setup(
         "lemur.plugins": ["gcp_dest = plugin_dest_gcp.plugin:GcpDestination"],
     },
     install_requires=[
-        "google-api-python-client >= 1",
-        "google-oauth >= 1",
-        "backoff >= 1.10",
+        "google-api-python-client == 1.*",
+        "google-oauth == 1.*",
+        "backoff == 1.10",
     ],
     packages=find_packages(),
     test_requires=(current / "requirements-test.txt").read_text().splitlines(),
