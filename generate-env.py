cc2bf225ba77d4ebad5d361c174eef5dc6d6e261
Attila Dulovics <attilad@spotify.com>
2021-03-18 16:49:11 +0100

    Added postgres default password

--- generate-env.py
+++ generate-env.py
@@ -13,6 +13,7 @@ sys.stdout.write(f"export SECRET_KEY={base64.b64encode(os.urandom(KEY_LENGTH)).d
 sys.stdout.write("export SQLALCHEMY_DATABASE_URI=postgresql://lemur:lemur@localhost:5432/lemur\n")
 sys.stdout.write("export REDIS_HOST=localhost\n")
 sys.stdout.write("export REDIS_PASSWORD=lemur\n")
+sys.stdout.write("export POSTGRES_PASSWORD=lemur\n")
 
 CONF_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lemur.conf.py")
 sys.stdout.write(f"export LEMUR_CONF={CONF_PATH}")
