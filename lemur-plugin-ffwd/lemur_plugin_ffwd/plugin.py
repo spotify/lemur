af023eb9771048b7580fff4119933480c9180994
Mattias Springare <mspringare@spotify.com>
2021-03-24 16:21:37 +0100

    add filter function to reduce metric cardinality (#127)

--- lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
+++ lemur-plugin-ffwd/lemur_plugin_ffwd/plugin.py
@@ -15,7 +15,17 @@ class FFWDMetricPlugin(MetricPlugin):
     def __init__(self, *args, **kwargs):
         self.mr = shumway.MetricRelay("lemur")
 
+    def filter_tags(self, tags=None):
+        if tags:
+            return {
+                k: v
+                for k, v in tags.items()
+                if k not in ["task_id", "receiver_hostname", "sender_hostname"]
+            }
+        return tags
+
     def submit(self, metric_name, metric_type, metric_value, metric_tags=None, options=None):
+        metric_tags = self.filter_tags(metric_tags)
         current_app.logger.debug(
             f"ffwd-plugin: sending metrics {metric_name} {metric_type} {metric_value} {metric_tags}"
         )
