78836b3ee7c41086034b38ede76ff5c8e31572fd
Mattias Springare <mspringare@spotify.com>
2021-03-26 12:38:51 +0100

    Use url_prefix
    from https://flower.readthedocs.io/en/latest/config.html#url-prefix

--- start-flower.sh
+++ start-flower.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flower --conf=/opt/lemur/celery-flower.conf.py --auth=.*@spotify\.com --broker=redis://:${REDIS_PASSWORD}@lemur-redis.services.gew1.spotify.net:6379/0
+flower --conf=/opt/lemur/celery-flower.conf.py --url_prefix=celery-flower --auth=.*@spotify\.com --broker=redis://:${REDIS_PASSWORD}@lemur-redis.services.gew1.spotify.net:6379/0
