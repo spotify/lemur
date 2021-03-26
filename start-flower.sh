9dd3d3c6e5ca7aecbb0a470a43afc70aec3e9c75
Mattias Springare <mspringare@spotify.com>
2021-03-26 14:11:09 +0100

    update oauth2_redirect_uri for flower

--- start-flower.sh
+++ start-flower.sh
@@ -6,5 +6,5 @@ flower \
   --auth=.*@spotify\.com \
   --oauth2_key=421791425557-lfk56lqi3rnhi4n2fr2rakmbv4lbc93l.apps.googleusercontent.com \
   --oauth2_secret=${GOOGLE_SECRET} \
-  --oauth2_redirect_uri=https://certs.spotify.net \
+  --oauth2_redirect_uri=https://certs.spotify.net/celery-flower/login \
   --broker=redis://:${REDIS_PASSWORD}@lemur-redis.services.gew1.spotify.net:6379/0
