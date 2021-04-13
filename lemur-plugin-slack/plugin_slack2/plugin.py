39cb3579a91a0268da3c77797e9c992327ab839f
Benjamin Greschbach <bgre@spotify.com>
2021-04-13 14:50:47 +0200

    lint

--- lemur-plugin-slack/plugin_slack2/plugin.py
+++ lemur-plugin-slack/plugin_slack2/plugin.py
@@ -1,10 +1,9 @@
 import logging
+
 import requests
 from flask import current_app
 from lemur.plugins.base import Plugin
-from lemur.plugins.bases import (
-    NotificationPlugin
-)
+from lemur.plugins.bases import NotificationPlugin
 
 logger = logging.getLogger(__name__)
 
@@ -90,17 +89,22 @@ ROTATION_TEMPLATE = """
 def _send_notification(data):
     bearer_token = _get_token()
     if not bearer_token:
-        logger.warning("SLACK_BOT_TOKEN not set, will not send any Slack notifications.")
+        logger.warning(
+            "SLACK_BOT_TOKEN not set, will not send any Slack notifications."
+        )
         return
 
     res = requests.post(
         "https://slack.com/api/chat.postMessage",
         headers={"Authorization": f"Bearer {bearer_token}"},
-        json=data)
+        json=data,
+    )
 
     if not res.ok:
-        logger.error("Failed to send Slack notification {res.text}",
-            extra=dict(result=res.json()))
+        logger.error(
+            "Failed to send Slack notification {res.text}",
+            extra=dict(result=res.json()),
+        )
 
 
 def _generate_rotation_notification(endpoint):
@@ -125,26 +129,22 @@ class SlackNotification(NotificationPlugin):
 
     @staticmethod
     def send_rotation_notification(channel, message, endpoint):
-        logger.info(
-            f"Sending rotation notification: {channel} {message} {endpoint}"
-        )
+        logger.info(f"Sending rotation notification: {channel} {message} {endpoint}")
 
         data = _generate_rotation_notification(endpoint)
 
-        _send_notification(
-            dict(
-                channel=channel,
-                text=data
-            ))
+        _send_notification(dict(channel=channel, text=data))
 
     @staticmethod
-    def send(notification_type, message, excluded_targets, options, endpoint=None, **kwargs):
+    def send(
+        notification_type, message, excluded_targets, options, endpoint=None, **kwargs
+    ):
         channel = Plugin.get_option("channel", options)
 
         if notification_type == "rotation":
             if endpoint is None:
                 logger.warning("Received rotation notification but endpoint was None")
                 return
-            return (
-                SlackNotification
-                .send_rotation_notification(channel, message, endpoint))
+            return SlackNotification.send_rotation_notification(
+                channel, message, endpoint
+            )
